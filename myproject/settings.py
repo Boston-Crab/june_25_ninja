0afe01e5b675a63d4de77117e0ff29cfbdc1a5a9
Matas <matassabaliauskas1@gmail.com>
2024-06-25 15:54:37 +0200

    bd

--- myproject/settings.py
+++ myproject/settings.py
@@ -98,7 +98,7 @@ if os.environ.get("GITHUB_WORKFLOW"):
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql",
-            "NAME": "github-actions",
+            "NAME": "monkey_bar_25",
             "USER": "postgres",
             "PASSWORD": "postgres",
             "HOST": "127.0.0.1",
